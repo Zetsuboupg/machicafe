d37df00887049bfea4461a995c38da79fb9fac36
EC2 Default User <ec2-user@ip-172-31-28-66.ap-northeast-1.compute.internal>
2024-08-18 10:37:53 +0000

    first deploy8

--- db/migrate/20240806090818_create_reviews.rb
+++ db/migrate/20240806090818_create_reviews.rb
@@ -3,7 +3,6 @@ class CreateReviews < ActiveRecord::Migration[6.1]
     create_table :reviews do |t|
       t.integer :user_id
       t.integer :cafe_id
-      t.integer :rating  # 1つの評価項目
       t.text :comment
 
       t.timestamps
